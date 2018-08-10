988653f6eba1f4394bf7a26e92e6e29bb20fc1ce
Juan David <jhuhandha@gmail.com>
2018-08-09 19:50:05 -0500

    subo cambio

--- api/api.js
+++ api/api.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send('Bienvenido al API REST del SENA');
+    res.send('Bienvenido API REST del SENA');
 });
 
 app.use(require("./routes/indexRoutes"));
